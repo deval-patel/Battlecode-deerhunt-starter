6c2845d6c4112d23817c3302c2c242cdb569a637
Riddhesh Shah <riddhesh.shah@mail.utoronto.ca>
2021-01-04 06:05:13 -0500

    Trying to move

---
+++ test_client/grid_player.py
@@ -0,0 +1,10 @@
+class GridPlayer:
+
+    def __init__(self):
+        self.foo = True
+
+    def tick(self, game_map, your_units, enemy_units, resources, turns_left):
+        out_lst = []
+        for unit in your_units:
+            print(unit.location)
+        return []
